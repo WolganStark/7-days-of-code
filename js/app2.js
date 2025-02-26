d14f0d872bd09a7b4d105935d2e8e07acaf1ee28
WolganStark <ing.staffelbach@gmail.com>
2025-02-26 16:50:55 -0500

    Terminación del día 2

--- js/app2.js
+++ js/app2.js
@@ -25,17 +25,28 @@ function preguntar(){
     if (contador === 0){
         document.getElementById('preguntas').innerHTML = preguntaNombre;
     } else{
-    if (contador === 1){
-        document.getElementById('preguntas').innerHTML = preguntaEdad;
-    } else{
-    if (contador === 2){
-        document.getElementById('preguntas').innerHTML = preguntaEstudio;
-    } else{
-        document.getElementById('preguntas').innerHTML = 'Gracias por participar';
-        document.getElementById('respuestas').innerHTML = `Hola ${respuestas[0]}, tienes ${respuestas[1]} años y ya estás aprendiendo ${respuestas[2]}`;
-    }
-    }
-    }   
+        if (contador === 1){
+            document.getElementById('preguntas').innerHTML = preguntaEdad;
+        } else{
+            if (contador === 2){
+                document.getElementById('preguntas').innerHTML = preguntaEstudio;
+        } else{
+            if(contador === 3){
+            document.getElementById('preguntas').innerHTML = `¿Te gusta estudiar ${respuestas[2]}? Responde con el número 1 para SI o 2 para NO`;        
+            document.getElementById('respuestas').innerHTML = `Hola ${respuestas[0]}, tienes ${respuestas[1]} años y ya estás aprendiendo ${respuestas[2]}.`;        
+            }
+        } if (contador === 4){
+            if (respuestas[3] === '1'){
+                document.getElementById('respuestas2').innerHTML = ` ¡Muy bien! Sigue estudiando y tendrás mucho exito`;
+                document.getElementById('button').setAttribute('disabled','true');
+                } else{
+                    document.getElementById('respuestas2').innerHTML += ` Oh, que pena... ¿Ya intentaste aprender otros lenguajes?`;
+                    document.getElementById('button').setAttribute('disabled','true');                        
+                }
+            }   
+        }
+    }    
+    return;
 }
 
 preguntar();
